ea57ff7c839d429c4dfe9ea4d96244bf1d154fe2
Yash Jadhav <yashjadhav1411@rocketmail.com>
2024-04-30 14:19:36 +0530

    add ui

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import racesResponse from './assets/data/races.json'
+import RaceListItem from './src/components/RaceListItem';
+
+
 
 export default function App() {
+
+  const races = racesResponse.data.races.response
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={races}
+        renderItem={({ item }) => <RaceListItem item={item} />}
+        />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'whitesmoke',
   },
+
 });
---
+++ src/components/RaceListItem.tsx
@@ -0,0 +1,80 @@
+import { Entypo } from '@expo/vector-icons';
+
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import racesResponse from '../../assets/data/races.json'
+import color from '../../assets/color';
+const races = racesResponse.data.races.response
+
+const RaceListItem = ({item}:{item : (typeof races[0])}) => {
+    const date = new Date(item.date)
+    const day = date.getDate().toString().padStart(2, '0')
+    const month = date.getMonth().toString().padStart(2, '0')
+    return (
+      <View style={styles.itemContainer}>
+        <View style={styles.contentContainer}>
+        <View>
+
+        <Text style={styles.date}>{`${day}-${month}`}</Text>
+        <Text style={styles.month}>NOV</Text>
+        </View>
+        <View style={styles.line} />
+        <View>
+        <Text style={styles.country}>{item.competition.location.country}</Text>
+
+        <Text style={styles.name}>Formula 1 {item.competition.name}</Text>
+        </View>
+        </View>
+        <Entypo name="chevron-right" size={24} color={color.primary} />
+      </View>
+    )
+  }
+
+  export default RaceListItem
+
+  const styles = StyleSheet.create({
+
+  contentContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    gap: 10,
+  },
+  itemContainer:{
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    margin: 5,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    borderRadius: 10,
+    shadowColor: "#000",
+shadowOffset: {
+	width: 0,
+	height: 2,
+},
+shadowOpacity: 0.25,
+shadowRadius: 3.84,
+
+elevation: 5,
+  },
+  line:{
+    height: 50,
+    width: 1,
+    backgroundColor: 'grey',
+    // marginHorizontal: 10,
+  },
+ 
+  country:{
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  date:{
+  },
+  month:{
+  },
+  name:{
+    color:'dimgray',
+  }
+})
